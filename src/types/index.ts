93281a72546ea3caae58acd7bbbd8bb24654cb22
Habid B Espinosa <hab.bad@hotmail.com>
2024-10-18 15:09:21 +0200

    fetching recipe detaills in store zustand, more schemas added with zos for API response, state umpdating properly

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,7 @@ import {
   CategoriesAPIResponseSchema,
   DrinkAPIResponse,
   DrinksAPIResponse,
+  RecipeAPIResponseSchema,
   searchFilterSchema,
 } from "../utils/recipies-schema";
 
@@ -10,3 +11,4 @@ export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof searchFilterSchema>;
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
 export type Drink = z.infer<typeof DrinkAPIResponse>;
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>;
